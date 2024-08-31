b499ea23def1e83b29bd706c5697e2d50c284efc
Abhishek-km <kumar.kabhi2000@gmail.com>
2024-09-01 00:53:24 +0530

    Created the Game, where the user can play the game single time in a go.

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+
+	// Print the welcome message of the Game
+	fmt.Println("Welcome to the Number Guessing Game!")
+	fmt.Println("I'm thinking of a number between 1 and 100.")
+	fmt.Println("You have 5 chances to guess the correct number.")
+
+	randomNumber := rand.Intn(100) + 1
+	fmt.Println(randomNumber)
+
+	// Print the levels of the game
+	fmt.Println("")
+	fmt.Println("Please select the difficulty level:")
+	fmt.Println("1. Easy (10 chances)")
+	fmt.Println("2. Medium (5 chances)")
+	fmt.Println("3. Hard (3 chances)")
+
+	// Take the input of the user regarding the diffuculity level
+	var difficultyLevel int
+	fmt.Print("\nEnter your choice:")
+	_, err := fmt.Scan(&difficultyLevel)
+	if err != nil {
+		fmt.Println("Error reading the input", err)
+		return
+	}
+
+	if difficultyLevel < 1 || difficultyLevel > 3 {
+		fmt.Println("Wrong choice, Restart the game.")
+	}
+
+	// Make variable to store the difficultiy level string value and no. of chances and assign value
+	var difficultyLevelString string
+	var noOfChances int
+
+	switch difficultyLevel {
+	case 1:
+		difficultyLevelString = "Easy"
+		noOfChances = 10
+	case 2:
+		difficultyLevelString = "Medium"
+		noOfChances = 5
+	case 3:
+		difficultyLevelString = "Hard"
+		noOfChances = 3
+	}
+
+	fmt.Printf("\nGreat! You have selected the %s difficulty level.", difficultyLevelString)
+	fmt.Printf("\nLet's start the game!")
+
+	for i := 1; i <= noOfChances; i++ {
+
+		// take the input of the guessed number
+		var gussedNumber int
+		fmt.Println("")
+		fmt.Print("\nEnter your guess:")
+		_, err := fmt.Scan(&gussedNumber)
+		if err != nil {
+			fmt.Println("Error reading the input", err)
+			return
+		}
+
+		if gussedNumber > randomNumber {
+			fmt.Printf("Incorrect! The number is less than %v", gussedNumber)
+		} else if gussedNumber < randomNumber {
+			fmt.Printf("Incorrect! The number is greater than %v", gussedNumber)
+		} else {
+			fmt.Printf("Congratulations! You guessed the correct number in %v attempts.", i)
+			fmt.Println()
+			return
+		}
+	}
+}
